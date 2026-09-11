13fe65d1e1151f8b1b12a2a6e6b9f21375dd5672
piku ashk <pikuashk@gmail.com>
2026-09-11 15:41:14 -0400

    Refactor averageAbove function for better input handling
    Improved input handling with error checking and added comments for clarity.

--- activity_3_2.py
+++ activity_3_2.py
@@ -1,11 +1,17 @@
 def averageAbove(threshold):
-  list = []
-  while True:
-    number = int(input("Number?"))
-    if int(number) < 0:
-      break
-    list.append(number)
-  listAbove = [item for item in list if item > threshold]
-  return int(sum(listAbove)/len(listAbove))
+    # Initialize list which stores the numbers
+    list = []
 
-averageAbove(5)
+  # Repeatedly ask for numbers until interrupted by <0 number
+    while True:
+    # Account for bad input
+        try:
+            number = int(input("Number?"))
+            if int(number) < 0:
+                break
+        except ValueError: # if user did not enter number
+            print("Enter a number!")
+        # add number
+        list.append(number)
+    listAbove = [item for item in list if item > threshold]  # iterate to make numbers greater than
+    return int(sum(listAbove)/len(listAbove))
