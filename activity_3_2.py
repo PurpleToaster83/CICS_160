0b1ba46edf1582a3be66035dbfbf7d8de56121eb
piku ashk <pikuashk@gmail.com>
2026-09-11 15:21:20 -0400

    Implement averageAbove function to calculate average

---
+++ activity_3_2.py
@@ -0,0 +1,11 @@
+def averageAbove(threshold):
+  list = []
+  while True:
+    number = int(input("Number?"))
+    if int(number) < 0
+      break
+    list.append(number)
+  listAbove = [item for item in list if item > threshold]
+  return int(sum(listAbove)/len(listAbove))
+
+averageAbove(5)
