d210a34321d5b0b77b791c29bb322994ba0f9aa7
piku ashk <pikuashk@gmail.com>
2026-09-25 15:23:37 -0400

    Add tree object and collision detection
    Added a tree object and collision detection with the car.

--- week3/Lab2_code/main.py
+++ week3/Lab2_code/main.py
@@ -3,11 +3,15 @@ import Vehicle
 
 def main():
     pygame.init()
-    canvas = pygame.display.set_mode((1240, 820))
-    car1 = Vehicle.Vehicle("./Images/green_car.png")
+    canvas      = pygame.display.set_mode((1240, 820))
+    car1        = Vehicle.Vehicle("./Images/green_car.png")
+    tree        = Vehicle.Vehicle("./Images/tree.png")
     keepRunning = True
     while (keepRunning):
         canvas.blit(car1.getImage(),car1.getPosition())
+        canvas.blit(tree.getImage(),tree.getPosition())
+        print(car1.IsCollidingWith(tree))
+
         pygame.display.flip()
         pressedKeys =  pygame.key.get_pressed()
         if pressedKeys[pygame.K_d]:
