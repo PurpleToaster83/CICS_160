8d8ac978d460f0814a23df37a75c56bbb0812a4c
William Van Uitert <william.vanuitert@gmail.com>
2026-09-25 14:35:32 -0400

    lab 2 starter files

---
+++ week3/Lab2_code/main.py
@@ -0,0 +1,21 @@
+import pygame
+import Vehicle
+
+def main():
+    pygame.init()
+    canvas      = pygame.display.set_mode((1240, 820))
+    car1        = Vehicle.Vehicle("./Images/green_car.png")
+    keepRunning = True
+    while (keepRunning):
+        canvas.blit(car1.getImage(),car1.getPosition())
+        pygame.display.flip()
+        pressedKeys =  pygame.key.get_pressed()
+        if pressedKeys[pygame.K_d]:
+            car1.setPosition(car1.getX() + 1, car1.getY())
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                keepRunning = False
+
+
+if __name__ == "__main__":
+    main()
